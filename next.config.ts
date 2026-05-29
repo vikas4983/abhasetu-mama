742a20f64a8b2e9527f8982be82759542c0e67dd
Vikas4983 <vikaskumarofficial2023@gmail.com>
2026-05-30 04:34:21 +0530

    github deployment setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Enables static HTML export
+  trailingSlash: true, // Ensures /about → /about/index.html
+  images: {
+    unoptimized: true, // Required for static export (no image server)
+  },
 };
 
 export default nextConfig;
